33e56dd16a17600e547bedb85fd5c08d13813a6c
prerana1821 <prerananawar1@gmail.com>
2024-02-23 03:08:05 +0530

    feat: audio analyzer

--- script.js
+++ script.js
@@ -7,12 +7,9 @@ const tool = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-input.addEventListener("change", (event) => {
+input.addEventListener("change", () => {
   const file = input.files[0];
-
-  if (!file) {
-    return;
-  }
+  if (!file) return;
 
   audio.src = URL.createObjectURL(file);
   audio.play();
@@ -23,4 +20,49 @@ input.addEventListener("change", (event) => {
   // 2. Create Audio Source
   // 3. Create Audio Effects (in our case, we'll analyze)
   // 4. Create Audio Destination
+
+  // Audio context processing graph or simple modular route
+  const audioContext = new AudioContext();
+
+  // Source Node
+  const audioSource = audioContext.createMediaElementSource(audio);
+
+  // Analyzer Node
+  const analyser = audioContext.createAnalyser();
+
+  audioSource.connect(analyser);
+
+  // Destination Node
+  analyser.connect(audioContext.destination); // Hardware speaker
+
+  analyser.fftSize = 512; // determines the number of frequency bins, which essentially translates to the number of data points used for the frequency analysis.
+  const bufferDataLength = analyser.frequencyBinCount; // retrieves the number of frequency bins that the analyser node will generate during the analysis. represents how many bars or data points we'll have for visualization.
+
+  const bufferDataArr = new Uint8Array(bufferDataLength); //to store the frequency data obtained from the analyser node. The length of this array is set to bufferDataLength, which represents the number of frequency bins.
+
+  const barWidth = canvas.width / bufferDataLength;
+  let x = 0;
+
+  function drawAndAnimateSoundBars() {
+    x = 0;
+    tool.clearRect(0, 0, canvas.width, canvas.height);
+
+    analyser.getByteFrequencyData(bufferDataArr); //retrieves frequency data from the analyser node and stores it in the bufferDataArr
+
+    bufferDataArr.forEach((dataValue) => {
+      const barHeight = dataValue;
+
+      const red = (barHeight * 2) % 150;
+      const green = (barHeight * 5) % 200;
+      const blue = (barHeight * 7) % 120;
+
+      tool.fillStyle = `rgb(${red}, ${green}, ${blue})`;
+      tool.fillRect(x, canvas.height - barHeight, barWidth, barHeight);
+      x += barWidth; // ensures that each successive sound bar is drawn at a position to the right of the previous one
+    });
+
+    if (!audio.ended) requestAnimationFrame(drawAndAnimateSoundBars);
+  }
+
+  drawAndAnimateSoundBars();
 });
